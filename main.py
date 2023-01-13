a3864e0d92f7da411304fe00c9c21f4c37405d67
gokul (Partly hacker) <91131376+3422222222@users.noreply.github.com>
2023-01-13 09:49:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def wishMe():
     else:
         speak("Good Evening!")
 
-    speak("I am zeus , Sir. Please tell me how may I help you")
+    speak("I am amigo , Sir. Please tell me how may I help you")
 
 def takeCommand():
     #It takes microphone input from the user and returns string output
